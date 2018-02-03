b6574afab5a411d7a536a26fd75299f403e43142
SilanHe <silanhe@outlook.com>
2018-02-03 14:46:05 -0500

    installed axios

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+          <p>User Token</p>
+          <input placeholder="Enter your user token" type="text"/>
       </div>
     );
   }
