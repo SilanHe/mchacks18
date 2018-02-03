0babbc1edb1fec2b36694dcfe77752d7d89a7e0b
Michel Ma <michel_ma@hotmail.ca>
2018-02-03 15:08:38 -0500

    added input component

---
+++ src/Graph.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import "./Graph.css";
+import {BarChart} from 'react-easy-chart';
+
+class Graph extends Component{
+	constructor(props){
+		super(props);
+	}
+
+	render(){
+		return(
+			<div>
+			hello
+			</div>
+		);
+	}
+}
+
+export default Graph;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Graph from './Graph';
+import Input from './Input';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Input />, document.getElementById('root'));
 registerServiceWorker();
