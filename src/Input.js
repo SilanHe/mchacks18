90c4ade4c1c798c88057948e3efb4701b159d1be
Michel Ma <michel_ma@hotmail.ca>
2018-02-03 19:22:47 -0500

    asdsad

--- src/Input.js
+++ src/Input.js
@@ -9,7 +9,7 @@ class Input extends Component{
 		super(props);
 		this.state = {
 			exampleData: [],
-			value: ''
+			authToken: ''
 		};
 
 		this.handleChange = this.handleChange.bind(this);
@@ -18,14 +18,13 @@ class Input extends Component{
 
 
 	handleChange(event){
-		this.setState({value : event.target.value});
+		this.setState({authToken : event.target.value});
 	}
 
 	handleSubmit(event){
-		axios.get("https://api.ciscospark.com/v1/rooms", {headers: {Authorization: 'Bearer '.concat(this.state.value)}}).then(response => {
+		axios.get("https://api.ciscospark.com/v1/rooms", {headers: {Authorization: 'Bearer '.concat(this.state.authToken)}}).then(response => {
 			const example = response.data.items;
 			this.setState({exampleData : example});
-			console.log(example);
 		})
 		.catch((error =>{
 			console.log('error 3 ' + error);
@@ -34,19 +33,20 @@ class Input extends Component{
 	}
 
 	render(){
+		
 		return(
 			<div>
 				<form onSubmit={this.handleSubmit}>
 					<label>
 						User token:
-						<input type="text" value={this.state.value} onChange={this.handleChange} />
+						<input type="text" value={this.state.authToken} onChange={this.handleChange} />
 					</label>
 					<input type="submit" value="Submit"/>
 				</form>
 
 				<ul>
 				{this.state.exampleData.map(item =>
-					<li key={item.id}> <RoomItem roomName={item.title} /></li>)}
+					<li key={item.id}> <RoomItem roomName={item.title} roomId={item.id} authToken={this.state.authToken} /></li>)}
 				</ul>
 			</div>
 		);
--- src/RoomItem.js
+++ src/RoomItem.js
@@ -1,26 +1,63 @@
 import React, { Component } from 'react';
 import "./RoomItem.css";
 import axios from 'axios';
+import StatsCard from './StatsCard';
 
 class RoomItem extends Component{
 	constructor(props){
 		super(props);
 
-		this.state = {listOfMessages:[]};
+		this.state = {listOfMessages:[], dataToDisplay: false, users: {}};
 
 		this.handleClick = this.handleClick.bind(this);
+		this.callbackFunc = this.callbackFunc.bind(this);
 
 	}
 
+	callbackFunc(response) {
+    // do something with the response
+    	console.log(response);
+	}
+
+
 	handleClick(){
-		console.log("clicked");
+		axios.get("https://api.ciscospark.com/v1/messages?roomId=".concat(this.props.roomId), {headers: {Authorization: 'Bearer '.concat(this.props.authToken)}}).then(response => {
+			const example = response.data.items;
+			var users = {}
+			for (var i = 0; i < example.length; i++){
+				if (example[i].personEmail in users){
+					users[example[i].personEmail] = users[example[i].personEmail] + 1;
+				}else{
+					users[example[i].personEmail] = 1;
+				}
+			}
+			this.setState({dataToDisplay: true, listOfMessages : example, users: users});
+		})
+		.catch(error =>{
+			console.log('error 3 ' + error);
+		});
+	}
+
+	activeUsers(){
+		var users = {}
+		for (var i = 0; i < this.state.listOfMessages.length; i++){
+			users[this.state.listOfMessages[i].personEmail] = 1;
+		}
 	}
 
 	render(){
+		var statsCard = (
+			<StatsCard  users={this.state.users} />
+		);
 		return(
+			<div>
 			<button onClick={this.handleClick}>
 			 {this.props.roomName}
-			 </button>
+			</button>
+
+			{this.state.dataToDisplay ? statsCard : null}
+			 
+			</div>
 		)
 	}
 }
